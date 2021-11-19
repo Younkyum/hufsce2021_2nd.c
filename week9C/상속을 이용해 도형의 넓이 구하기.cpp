1066ef738b685229d96f1d2640003e53bfe5dfc8
YounKyum Jin <83539914+Younkyum@users.noreply.github.com>
2021-11-19 15:56:08 +0900

    Update 상속을 이용해 도형의 넓이 구하기.cpp

--- week9C/상속을 이용해 도형의 넓이 구하기.cpp
+++ week9C/상속을 이용해 도형의 넓이 구하기.cpp
@@ -4,9 +4,80 @@ using namespace std;
 const float pi=3.14;
 
 class Shape {
-private:
+public:
 	int x;
 	int y;
+};
+
+class Square : Shape {
+private:
+	int width;
+	int height;
+public:
+	Square();
+	Square(int xpoint, int ypoint, int width, int height) {
+		x = xpoint;
+		y = ypoint;
+		this->width = width;
+		this->height = height;
+	}
+	float getArea() {
+		float ar;
+		ar = height * width;
+		return ar;
+	}
+	void print() {
+		cout << "shape at (" << x << ", " << y << ") : width=" << width << " height=" << height;
+		cout << fixed;
+		cout.precision(2);
+		cout << " area=" << getArea() << endl;
+	}
+};
+class Circle : Shape {
+private:
+	flaot radius;
+public:
+	Circle();
+	Circle(int xpoint, int ypoint, flaot radius) {
+		x = xpoint;
+		y = ypoint;
+		this->radius = radius;
+	}
+	float getArea() {
+		float ar;
+		ar = radius*radius*pi;
+		return ar;
+	}
+	void print() {
+		cout << "shape at (" << x << ", " << y << ") : radius=" << radius;
+		cout << fixed;
+		cout.precision(2);
+		cout << " area=" << getArea() << endl;
+	}
+};
+class Triangle : Shape {
+private:
+	int width;
+	int height;
+public:
+	Triangle();
+	Triangle(int xpoint, int ypoint, int width, int height) {
+		x = xpoint;
+		y = ypoint;
+		this->width = width;
+		this->height = height;
+	}
+	float getArea() {
+		float ar;
+		ar = width * height * 0.5;
+		return ar;
+	}
+	void print() {
+		cout << "shape at (" << x << ", " << y << ") : width=" << width << " height=" << height;
+		cout << fixed;
+		cout.precision(2);
+		cout << " area=" << getArea() << endl;
+	}
 };
 
 

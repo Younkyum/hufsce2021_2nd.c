43e3e1bae4253c365b6aa511bd830e26ba289b0b
YounKyum Jin <83539914+Younkyum@users.noreply.github.com>
2021-11-25 21:19:24 +0900

    Update 은행계좌Final.cpp

--- programming_assignment_4/은행계좌Final.cpp
+++ programming_assignment_4/은행계좌Final.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
+#define MAX 1000
 using namespace std;
+
 
 class Account {
 public:
@@ -42,11 +44,12 @@ public:
 		number = 111111;
 		this->count = count;
 		this->nc = 0;
+		Account *bankacc = new Account[this->count];
 	}
 	int finder(int fnum) {
 		int reminder = -1;
 		for (int i = 0; i < count; i++) {
-			if (bankacc[i] == fnum) {
+			if (bankacc[i].accnum == fnum) {
 				reminder = i;
 			}
 		}
@@ -56,15 +59,83 @@ public:
 			return reminder;
 		}
 	}
+	int findnextlevel(Account a[], int fnum) {
+		int reminder = -1;
+		int c;
+		int min = 0;
+		for (int i = 0; i < count; i++) {
+			c = a[i].accnum;
+			if (c - fnum > min) {
+				reminder = i;
+				min = c - fnum;
+			}
+		}
+		if (min == 0 || reminder == -1) {
+			return -1;
+		} else {
+			return reminder;
+		}
+	}
 	void open(stirng name, int balance) {
 		Account a;
-		a.make(accnum, name, balance);
-		accnum++;
+		a.make(number, name, balance);
+		number++;
 		bankacc[nc] = a;
 		nc++;
 	}
 	void deposit(int num, int amount) {
-		
+		if (finer(num) == -1) {
+			cout << "Error1" << endl;
+		} else {
+			bankacc(finder(num)).income(amount);
+		}
+	}
+	void withdraw(int num, int amount) {
+		if(finder(num) == -1) {
+			cout << "Error2" << endl;
+		} else {
+			bankacc(finder(num)).outcome(amount);
+		}
+	}
+	void transfer(int num1, int num2, int amount) {
+		if (finder(num1) == -1 || finder(num2) == -1) {
+			cout << "Error1" << endl;
+		} else {
+			bankacc(finder(num1)).outcome(amount);
+			bankacc(finder(num2)).income(amount);
+		}
+	}
+	void printa(int num) {
+		bankacc(finder(num)).print();
+	}
+	void find_and_print(string fname) {
+		Account namer[MAX];
+		int basic = 111111;
+		int counter = 1;
+		for (int i = 0; i < this->count; i++) {
+			if (bankacc[i].accname == fname) {
+				namer[counter - 1] = bankacc[i];
+				counter++
+			}
+		}
+		if (counter == 1) {
+			cout << "Error1" << endl;
+		} else {
+			for (int i = 0; i < counter; i++) {
+				namer[findnextlevel(namer, basic)].print();
+				basic = namer[findnextlevel(namer, basic)].accnum;
+			}
+		}
+	}
+	void eraser(int num) {
+		int index;
+		if (finder(num) == -1) {
+			cout << "Error1" << endl;
+		} else {
+			index = finder(num);
+			bankacc[index] = bankacc[nc-1];
+			nc--;
+		}
 	}
 };
 

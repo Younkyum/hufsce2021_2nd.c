b907d2b1b81af76e7907fdb1f4b4f5eb630fa0af
YounKyum Jin <83539914+Younkyum@users.noreply.github.com>
2021-11-25 21:38:13 +0900

    update

--- programming_assignment_4/은행계좌Final.cpp
+++ programming_assignment_4/은행계좌Final.cpp
@@ -48,7 +48,7 @@ public:
 	}
 	int finder(int fnum) {
 		int reminder = -1;
-		for (int i = 0; i < count; i++) {
+		for (int i = 0; i < nc; i++) {
 			if (bankacc[i].accnum == fnum) {
 				reminder = i;
 			}
@@ -63,7 +63,7 @@ public:
 		int reminder = -1;
 		int c;
 		int min = 0;
-		for (int i = 0; i < count; i++) {
+		for (int i = 0; i < nc; i++) {
 			c = a[i].accnum;
 			if (c - fnum > min) {
 				reminder = i;
@@ -76,7 +76,7 @@ public:
 			return reminder;
 		}
 	}
-	void open(stirng name, int balance) {
+	void open(string name, int balance) {
 		Account a;
 		a.make(number, name, balance);
 		number++;
@@ -84,38 +84,38 @@ public:
 		nc++;
 	}
 	void deposit(int num, int amount) {
-		if (finer(num) == -1) {
+		if (finder(num) == -1) {
 			cout << "Error1" << endl;
 		} else {
-			bankacc(finder(num)).income(amount);
+			bankacc[finder(num)].income(amount);
 		}
 	}
 	void withdraw(int num, int amount) {
 		if(finder(num) == -1) {
 			cout << "Error2" << endl;
 		} else {
-			bankacc(finder(num)).outcome(amount);
+			bankacc[finder(num)].outcome(amount);
 		}
 	}
 	void transfer(int num1, int num2, int amount) {
 		if (finder(num1) == -1 || finder(num2) == -1) {
 			cout << "Error1" << endl;
 		} else {
-			bankacc(finder(num1)).outcome(amount);
-			bankacc(finder(num2)).income(amount);
+			bankacc[finder(num1)].outcome(amount);
+			bankacc[finder(num2)].income(amount);
 		}
 	}
 	void printa(int num) {
-		bankacc(finder(num)).print();
+		bankacc[finder(num)].print();
 	}
 	void find_and_print(string fname) {
 		Account namer[MAX];
 		int basic = 111111;
 		int counter = 1;
-		for (int i = 0; i < this->count; i++) {
+		for (int i = 0; i < this->nc; i++) {
 			if (bankacc[i].accname == fname) {
 				namer[counter - 1] = bankacc[i];
-				counter++
+				counter++;
 			}
 		}
 		if (counter == 1) {
@@ -137,9 +137,52 @@ public:
 			nc--;
 		}
 	}
+	void all_print() {
+		int basic = 111110;
+		for (int i = 0; i < count; i++) {
+			bankacc[findnextlevel(bankacc, basic)].print();
+			basic = bankacc[findnextlevel(bankacc, basic)].accnum;
+		}
+	}
 };
 
-
+int main() {
+	Bank bank(1000);
+	string command;
+	string name;
+	int accNo, amount;
+	int accNo1, accNo2;
+	
+	while (cin >> command) {
+		if (command == "신규") {
+			cin >> name >> amount;
+			bank.open(name, amount);
+		} else if (command == "입금") {
+			cin >> accNo >> amount;
+			bank.deposit(accNo, amount);
+		} else if (command == "출금") {
+			cin >> accNo >> amount;
+			bank.withdraw(accNo, amount);
+		} else if (command == "계좌이체") {
+			cin >> accNo1 >> accNo2 >> amount;
+			bank.transfer(accNo1, accNo2, amount);
+		} else if (command == "계좌번호조회") {
+			cin >> accNo;
+			bank.printa(accNo);
+		} else if (command == "예금주이름조회") {
+			cin >> name;
+			bank.find_and_print(name); 
+		} else if (command == "해지") {
+			cin >> accNo;
+			bank.eraser(accNo);
+		} else if (command == "전체계좌조회") {
+			bank.all_print();
+		} else if (command == "종료") {
+			cout << "종료합니다" << endl;
+			break;
+		}
+	}
+}
 
 
 
